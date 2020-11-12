29f19811e234e0b60b0f43d8cd597a66e813f2ce
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-11-12 12:15:14 -0500

    Create GameTest.java

---
+++ GameTest.java
@@ -0,0 +1,17 @@
+/**
+ 
+ * Class GameTest.
+ 
+ */
+public class GameTest {
+/**
+ * Class GameTest.
+ * 
+ */
+  public static void main(String[] args) {
+    GuessGame guessGame = new GuessGame();
+        
+    //called startGame()
+    guessGame.startGame();
+  }
+}
